a11148189be9352459bcceaf94974f855c55578f
Maksym-Kovorotnyi <Kovorotny1@gmail.com>
2023-04-04 14:55:33 +0300

    add basename

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { store } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename="/Kids_Like">
       <App />
     </BrowserRouter>
   </Provider>
