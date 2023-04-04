e96b50d0a37b974d3e5443fb5ab93c394a952b23
Maksym-Kovorotnyi <Kovorotny1@gmail.com>
2023-04-04 17:01:25 +0300

    Update AwardSlice.js

--- src/redux/Award/AwardSlice.js
+++ src/redux/Award/AwardSlice.js
@@ -1,72 +1,7 @@
 const { createSlice } = require('@reduxjs/toolkit');
 
 const awardInitialState = {
-  ruGifts: [
-    {
-      title: 'Sweets',
-      price: 40,
-      imageUrl:
-        'https://storage.googleapis.com/kidslikev2_bucket/Rectangle%2025%20(8).png',
-      isSelected: false,
-      id: 1,
-    },
-    {
-      title: 'Go to the movie',
-      price: 90,
-      imageUrl:
-        'https://storage.googleapis.com/kidslikev2_bucket/Rectangle%2025%20(9).png',
-      isSelected: false,
-      id: 2,
-    },
-    {
-      title: 'Gift',
-      price: 100,
-      imageUrl:
-        'https://storage.googleapis.com/kidslikev2_bucket/Rectangle%2025%20(10).png',
-      isSelected: false,
-      id: 3,
-    },
-    {
-      title: 'Pizza evening',
-      price: 90,
-      imageUrl:
-        'https://storage.googleapis.com/kidslikev2_bucket/Rectangle%2025%20(11).png',
-      isSelected: false,
-      id: 4,
-    },
-    {
-      title: 'Party with friends',
-      price: 120,
-      imageUrl:
-        'https://storage.googleapis.com/kidslikev2_bucket/Rectangle%2025%20(12).png',
-      isSelected: false,
-      id: 5,
-    },
-    {
-      title: 'Go to the Mac',
-      price: 80,
-      imageUrl:
-        'https://storage.googleapis.com/kidslikev2_bucket/Rectangle%2025%20(13).png',
-      isSelected: false,
-      id: 6,
-    },
-    {
-      title: 'Wish',
-      price: 200,
-      imageUrl:
-        'https://storage.googleapis.com/kidslikev2_bucket/Rectangle%2025%20(14).png',
-      isSelected: false,
-      id: 7,
-    },
-    {
-      title: 'Go to the katok',
-      price: 100,
-      imageUrl:
-        'https://storage.googleapis.com/kidslikev2_bucket/Rectangle%2025%20(15).png',
-      isSelected: false,
-      id: 8,
-    },
-  ],
+  giftIds: [],
 };
 
 const awardSlice = createSlice({
